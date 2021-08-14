79caf0142d23c5c2cf8ef795e6d101b1f89c798e
Khalida Begum <k.begum1@gmail.com>
2021-08-14 12:10:06 +0200

    error message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,125 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// My part:Fermer la modale
 
+//element à cherché et modifier
+
+const closeBtn = modalbg.querySelector(".close");
+const modalContent = modalbg.querySelector(".content");
+const modalBody = modalbg.querySelector(".modal-body");
+const subscriptionForm = document.getElementById("subscription-form");
+
+//création function pour fermer modal
+
+closeBtn.addEventListener("click", closeModal);
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+//My part: Implémenter entrées du formulaire
+
+//constraints et conditions a remplir pour pouvoir valider
+function isValidFirstName(first) {
+  return first.length >= 2;
+}
+
+function isValidLastName(last) {
+  return last.length >= 2;
+}
+
+function isValidTournamentNumber(tournamentnumber) {
+  return tournamentnumber >= 0;
+}
+
+function isValidEmail(email) {
+  const emailShape =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailShape.test(email);
+}
+
+function isValidDate(datestr) {
+  //  convertir string en date
+  const date = new Date(datestr);
+  // recuperer le time date (si pas de nombre(isNAN) date nest pas bon)
+  const time = date.getTime();
+  if (isNaN(time)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function oneIsChecked(checkedList) {
+  return checkedList.includes(true);
+}
+
+/*function conditionsAccepted(){
+  const conditionIsChecked = document.getElementById("checkbox1").checked;
+  if (conditionIsChecked){
+    return true;
+  } else{
+    return false;
+  }
+}
+*/
+
+function validate() {
+  const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  if (isValidFirstName(firstName) && isValidLastName(lastName)) {
+    return true;
+  }
+  return false;
+}
+const errorMessages = {
+  firstname: "Must contain at least 2 letters",
+  lastname: "Must contain at least 2 letters",
+  email: "Please enter a valid e-mail adresse",
+  birthdate: "Please enter a valid birth date",
+  quantity: "Please enter a number superior to 0",
+  location: "Please select a city ",
+  checkbox: "You must agree to terms and conditions",
+};
+function createErrorMessage(id, message) {
+  const errorMsg = document.createElement("span");
+  errorMsg.textContent = message;
+  document.getElementById(id).parentElement.appendChild(errorMsg);
+  errorMsg.classList.add("message-error");
+}
+
+createErrorMessage("first", errorMessages.firstname);
+createErrorMessage("last", errorMessages.lastname);
+createErrorMessage("email", errorMessages.email);
+createErrorMessage("birthdate", errorMessages.birthdate);
+createErrorMessage("quantity", errorMessages.quantity);
+createErrorMessage("location1", errorMessages.location);
+createErrorMessage("checkbox1", errorMessages.checkbox);
+
+/*
+const errorMessages = {
+  firstname:"Must contain at least 2 letters",
+  lastName:"Must contain at least 2 letters",
+  email:"Please enter a valid e-mail adresse",
+  birthdate: "Please enter a valid birth date",
+  quantity:"Please enter a number superior to 0",
+  location:"Please select a city ",
+  checkbox:"You must agree to terms and conditions",
+}
+
+
+/*
+ document.createElement('span').textContent='message-error';
+ parentElement.querySelector(".message-error").style.display = "inline";
+
+
+ function displayError(error) {
+   const errorAlert= document.createElement('span')='message-error'.textcontent;
+   errorAlert.classlist.add("message-error");
+
+}*/
+
+//My part: Message error
+
+/* document.createElement('span')= "message-error"
+const error= document.createElement */
